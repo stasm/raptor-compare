2ae996aeeb6b3994e01cc5ce93424838b9529c55
Staś Małolepszy <stas@mozilla.com>
2015-10-10 01:51:15 +0200

    gulp lint

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,19 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
 
 gulp.task('test', function () {
   return gulp.src('test/*.js', { read: false })
-             .pipe(mocha({ ui: 'tdd' }));
+    .pipe(mocha({ ui: 'tdd' }));
 });
 
+gulp.task('lint', function () {
+  return gulp.src(['src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint', 'test'], function () {
+});
