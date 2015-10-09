e3467381aef79aa62e95371c0a320171a0d1ff70
Staś Małolepszy <stas@mozilla.com>
2015-10-09 14:19:55 +0200

    add revisions

--- src/parse.js
+++ src/parse.js
@@ -12,8 +12,9 @@ module.exports = function(obj) {
     {});
 };
 
-const newmap = () => new Map();
-const newarr = () => [];
+const newOrigin = () => new Map();
+const newSeries = (rev) => new Map([['rev', rev], ['data', new Map()]]);
+const newValues = () => [];
 
 function get(map, key, def) {
   if (map.has(key)) {
@@ -27,11 +28,12 @@ function get(map, key, def) {
 
 function parse(results, point) {
   const origin = get(
-    results, point.tags.context, newmap);
+    results, point.tags.context, newOrigin);
   const series = get(
-    origin, point.timestamp.slice(0, point.timestamp.length - 6), newmap);
+    origin, point.timestamp.slice(0, point.timestamp.length - 6),
+    () => newSeries(point.tags.revisionId));
   const values = get(
-    series, point.tags.metric, newarr);
+    series.get('data'), point.tags.metric, newValues);
   values.push(point.fields.value);
   return results;
 }
--- src/table.js
+++ src/table.js
@@ -4,29 +4,35 @@ const ttest = require('ttest');
 const summary = require('summary');
 const Table = require('easy-table');
 
+function shortrev(rev) {
+  return rev.slice(0, 4);
+}
+
 function printPValue(val, width) {
   const sig = val < 0.05 ? '* ' : '  ';
   return Table.padLeft(sig + val.toFixed(2), width);
 }
 
 module.exports = function(origin, baseSeries, ...tries) {
-  return Array.from(baseSeries.keys()).reduce((t, mark) => {
+  return Array.from(baseSeries.get('data').keys()).reduce((t, mark) => {
     t.cell(origin, mark);
 
     const numFormat = ['uss', 'pss', 'rss'].includes(mark) ?
       Table.number(3) : Table.number(0);
 
-    const h0 = summary(baseSeries.get(mark));
-    t.cell('base', h0.mean(), numFormat);
+    const h0 = summary(baseSeries.get('data').get(mark));
+    const h0rev = shortrev(baseSeries.get('rev'));
+    t.cell('base: ' + h0rev, h0.mean(), numFormat);
 
     tries.forEach((trySeries, i) => {
-      i++;
-      const h1 = summary(trySeries.get(mark));
+      const num = (i + 1) + ': ';
+      const h1 = summary(trySeries.get('data').get(mark));
+      const h1rev = shortrev(trySeries.get('rev'));
       const delta = h1.mean() - h0.mean();
       const pval = ttest(h0._data, h1._data).pValue();
-      t.cell('try ' + i, h1.mean(), numFormat);
-      t.cell('delta ' + i, delta, numFormat);
-      t.cell('p-value ' + i, pval, printPValue);
+      t.cell(num + h1rev, h1.mean(), numFormat);
+      t.cell(num + 'delta', delta, numFormat);
+      t.cell(num + 'p-value', pval, printPValue);
     });
 
     return t.newRow();
